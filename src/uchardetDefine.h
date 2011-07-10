1b05009d4dc83d79741bfa496d86f2a6b1efac80
BYVoid <byvoid.kcp@gmail.com>
2011-07-10 15:43:28 +0800

    Update contributors information.

--- src/uchardetDefine.h
+++ src/uchardetDefine.h
@@ -19,7 +19,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *          Kohei TAKETA <k-tak@void.in>
+ *          BYVoid <byvoid.kcp@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
