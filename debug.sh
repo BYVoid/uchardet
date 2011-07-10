76a1be36f89c0fc6500d1b8f3da39833eb06707d
BYVoid <byvoid.kcp@gmail.com>
2011-07-10 16:52:59 +0800

    Refine release.sh & debug.sh.

--- debug.sh
+++ debug.sh
@@ -2,7 +2,6 @@ mkdir --parents debug \
 && cd debug
 
 cmake \
-	-D ENABLE_GETTEXT:BOOL=ON \
 	-DCMAKE_BUILD_TYPE=Debug \
 	-DCMAKE_INSTALL_PREFIX=`pwd`/root \
 	.. \
--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@ mkdir --parents release \
 && cd release
 
 cmake \
-	-D ENABLE_GETTEXT:BOOL=ON \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX=/usr \
 	.. \
