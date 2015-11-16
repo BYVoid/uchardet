BYVoid/uchardet

commit 3a518c0536e5986b740c583e4b71152a414ac90b

do not use std::string which breaks c++ abi
Some stl types can break abi. If the program is built with g++ 5, and libstdc++ on the target platform is g++ 4.x, then it can not run

src/uchardet.cpp
@@ -35,41 +35,47 @@
  *
  * ***** END LICENSE BLOCK ***** */
 #include "uchardet.h"
+#include <string.h>
+#include <stdlib.h>
 #include "nscore.h"
 #include "nsUniversalDetector.h"
-#include <string>
-
-using std::string;
 
 class HandleUniversalDetector : public nsUniversalDetector
 {
 protected:
-	string m_charset;
+    char *m_charset;
 
 public:
     HandleUniversalDetector()
     : nsUniversalDetector(NS_FILTER_ALL)
+    , m_charset(0)
     {
-        m_charset = "";
     }
 
     virtual ~HandleUniversalDetector()
-    {}
+    {
+        if (m_charset)
+            free(m_charset);
+    }
 
     virtual void Report(const char* charset)
     {
-        m_charset = charset;
+        if (m_charset)
+            free(m_charset);
+        m_charset = strdup(charset);
     }
 
     virtual void Reset()
     {
         nsUniversalDetector::Reset();
-        m_charset = "";
+        if (m_charset)
+            free(m_charset);
+        m_charset = strdup("");
     }
 
     const char* GetCharset() const
     {
-        return m_charset.c_str();
+        return m_charset;
     }
 };
 

commit 256d1957b2c66fd106ea2277e41252437f1c76ad

uchardet_get_charset() should never return NULL...
... to stay backward compatible with previous behavior.
About detection failure, our in-code documentation says:
"@return name of charset on success and "" on failure or pure ascii."
This behavior had been broken by commit 3a518c0, which returned NULL
instead. Our command-line tool was the first victim, segfaulting on
ASCII files.

src/uchardet.cpp
@@ -75,7 +75,7 @@ public:
 
     const char* GetCharset() const
     {
-        return m_charset;
+        return m_charset? m_charset : "";
     }
 };
 